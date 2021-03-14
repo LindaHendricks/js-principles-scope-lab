025964f3004f7647cdfaf2e5ec0d0528421831b3
Linda Hendricks <linda.timoh@gmail>
2021-03-13 22:07:05 -0500

    commit tree 91a465614bc1f78159b92c07c0f5b1e5e353e754

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+var customerName = `bob`;
+const leastFavoriteCustomer = `josh`
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = `not bob`;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = `maybe bob`
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Assignment to constant variable.';
+}
+
 // Write your solution in this file!
